acba8a134cc1dbc440e41df227bf5096d5637d19
daniel-willett <willettdr1@gmail.com>
2025-07-28 17:07:06 +0100

    Added first bit of functionality to get the current URL that the user is looking at. Next is to consider how to store this in a CSV and then to consider how to load those values onto the HTML popup

--- actions.js
+++ actions.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("addCurrentURLButton");
+const button = document.getElementById("addCurrentURLButton");
 
-  button.addEventListener("click", function () {
-    console.log("Button clicked!");
-    // Add your custom logic here
-  });
+button.addEventListener("click", function () {
+	// Got this from https://stackoverflow.com/questions/1979583/how-can-i-get-the-url-of-the-current-tab-from-a-google-chrome-extension/14251218#14251218
+	// Refer to this if things break
+	(async () => {
+	const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+	console.log(tab.url);
+	})();
+	
 });
